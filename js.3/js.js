064b383557c26ef47834712aebde10929c6577ca
Azizbek Komilov <komilovazizbek2562@gmail.com>
2025-11-29 18:28:11 +0500

    Add files via upload

---
+++ js.3/js.js
@@ -0,0 +1,30 @@
+// Massivlar
+const color = ['red', 'green', 'blue', 'yellow']
+console.log(color.length);
+console.log(color[3]);
+
+
+
+
+console.log(color.pop());
+console.log(color);
+
+
+
+console.log(color.push ('white'));
+console.log(color);
+
+
+console.log(color.shift());
+console.log(color);
+
+
+console.log(color.unshift('orenge'));
+console.log(color);
+
+
+
+
+
+
+
